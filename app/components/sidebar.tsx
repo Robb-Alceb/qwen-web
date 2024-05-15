23462a0450e0e21941477871fab7d1ef4b1f35f2
liushichang <shichang.liu@chainacreator.com>
2024-05-15 21:36:12 +0800

    wip: 去除面具、设置功能

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -166,7 +166,7 @@ export function SideBar(props: { className?: string }) {
       </div>
 
       <div className={styles["sidebar-header-bar"]}>
-        <IconButton
+        {/* <IconButton
           icon={<MaskIcon />}
           text={shouldNarrow ? undefined : Locale.Mask.Name}
           className={styles["sidebar-bar-button"]}
@@ -178,7 +178,7 @@ export function SideBar(props: { className?: string }) {
             }
           }}
           shadow
-        />
+        /> */}
         <IconButton
           icon={<PluginIcon />}
           text={shouldNarrow ? undefined : Locale.Plugin.Name}
@@ -211,11 +211,11 @@ export function SideBar(props: { className?: string }) {
               }}
             />
           </div>
-          <div className={styles["sidebar-action"]}>
+          {/* <div className={styles["sidebar-action"]}>
             <Link to={Path.Settings}>
               <IconButton icon={<SettingsIcon />} shadow />
             </Link>
-          </div>
+          </div> */}
           {/* <div className={styles["sidebar-action"]}>
             <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
               <IconButton icon={<GithubIcon />} shadow />
